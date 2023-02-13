b83f286c15fdb66ded4a3d2db592321db4081f50
tarekahmad <tarek.ahmed9710@gmail.com>
2023-02-13 21:21:47 +0200

    Initial commit

---
+++ src/main/resources/features/F01_Register.feature
@@ -0,0 +1,4 @@
+@smoke
+Feature: F01_Register | users could register with new accounts
+  Scenario: guest user could register with valid data successfully
+    Given user go to register page
---
+++ src/test/java/org/example/stepDefs/D01_registerStepDef.java
@@ -0,0 +1,9 @@
+package org.example.stepDefs;
+import io.cucumber.java.en.Given;
+public class D01_registerStepDef {
+    @Given("user go to register page")
+    public void goRegisterPage()
+    {
+        System.out.println("This is a test before start coding");
+    }
+}
